32560b1913a18b3b225755ee85e86095b7a77770
Noraelk <67266165+Noraelk@users.noreply.github.com>
2020-07-02 17:04:34 +0200

    day 7

--- EX_01/EX_02/utiliser-objets.php
+++ EX_01/EX_02/utiliser-objets.php
@@ -5,13 +5,13 @@ echo '<p>'.$date1->format('Y-m-d H:i:s').'</p>';
  
 
 $date2 = new DateTime('2020-06-22 9:00:00');
-echo '<p>'.$sate2->format('Y-m-d H:i:s').'</p>';
+echo '<p>'.$date2->format('Y-m-d H:i:s').'</p>';
  
 
-$interval = $date2->diff($sate1);
-echo $interval->format ('%R%a days');
+$interval = $date2->diff($date1);
+echo $interval->format('%R%a days');
  
-$interval = $date->diff($date1);
+$interval = $date2->diff($date1);
 echo '<p>'.$interval->format(' %a days');
 echo $interval->format(' %H hours');
 echo $inteval->format(' %i minutes').'</p>';
